0f2c819c3f8467241fff30be41c9659c128343ed
Ji-Rath <garethaguilar@gmail.com>
2021-12-21 13:25:11 -0600

    Integrate steam/epic crossplay
    Turns out the main issue was accidentally using Steam net drivers during P2P, oops
    Update launch bat files
    Add Epic/Steam DefaultEngine config files

--- LaunchUncooked - JiRath.bat
+++ LaunchUncooked - JiRath.bat
@@ -1,1 +1,1 @@
-"D:\Program Files\Epic Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe" "D:\Users\garet\Documents\Git\UEProjects\EOSPlusTestProject\EOSTestProject.uproject" -game -log -AUTH_TYPE="Developer" -AUTH_LOGIN="localhost:5421" -AUTH_PASSWORD="JiRath"
+start "" "D:\Program Files\Epic Games\UE_4.27\Engine\Binaries\Win64\UE4Editor.exe" "D:\Users\garet\Documents\Git\UEProjects\EOSPlusTestProject\EOSTestProject.uproject" -game -log -AUTH_TYPE="Developer" -AUTH_LOGIN="localhost:5421" -AUTH_PASSWORD="JiRath"
